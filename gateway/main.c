5a134400a4d259cd0f81bda00f1495d88d509bf0
Banksy <com-gitgub@paulbanks.org>
2021-03-25 10:44:43 +0000

    Remote flow control added to support remote firmware upgrade

--- gateway/main.c
+++ gateway/main.c
@@ -6,6 +6,8 @@
 */
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <sys/ioctl.h>
+#include <signal.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -17,16 +19,36 @@
 #define DEFAULT_SERIAL_PORT "/dev/ttyS1"
 #define BUF_SIZE 1024
 
+#define OOB_HW_FLOW_OFF 0x10
+#define OOB_HW_FLOW_ON 0x11
+
 static fd_set _master_read_set;
-static int _serial_fd;
+static fd_set _master_except_set;
+static fd_set _read_fd_set;
+static fd_set _except_fd_set;
+struct serial_settings {
+    bool is_hardware_flow_control; // True if hardware flow control
+    uint32_t baud_bps; // Baud rate. Must be valid (see serial.h)
+    char* device; // Path to serial device
+};
+static struct serial_settings _serial_settings;
+
+static int _serial_fd = -1;
 static int _connection_fd = -1;
 static int _buf[BUF_SIZE];
+
 
 #ifdef ENABLE_DEBUGLOG
 #define LOG_DEBUG(format, ...) fprintf(stderr, format "\n", __VA_ARGS__)
 #else
 #define LOG_DEBUG(format, ...)
 #endif
+
+int sockatmark(int fd)
+{
+    int r;
+    return ioctl(fd, SIOCATMARK, &r) == -1 ? -1 : r;
+}
 
 static void _set_status_led(bool is_on)
 {
@@ -52,34 +74,79 @@ static void  _close_connectionfd()
         shutdown(_connection_fd, SHUT_RDWR);
         close(_connection_fd);
         FD_CLR(_connection_fd, &_master_read_set);
+        FD_CLR(_connection_fd, &_read_fd_set);
+        FD_CLR(_connection_fd, &_master_except_set);
+        FD_CLR(_connection_fd, &_except_fd_set);
         _connection_fd = -1;
     }
 }
 
+static void _open_serial_port()
+{
+    if (_serial_fd != -1) {
+        FD_CLR(_serial_fd, &_master_read_set);
+        FD_CLR(_serial_fd, &_read_fd_set);
+        close(_serial_fd);
+    }
+    _serial_fd = serial_port_open(_serial_settings.device,
+                                  _serial_settings.baud_bps,
+                                  _serial_settings.is_hardware_flow_control);
+    if (_serial_fd==-1) {
+        _error_exit("Could not open serial port.");
+    }
+    FD_SET(_serial_fd, &_master_read_set);
+}
+
+static void _handle_oob_command()
+{
+    char oob_op;
+    size_t len = recv(_connection_fd, &oob_op, 1, MSG_OOB);
+    if (len==1) {
+        switch (oob_op) {
+            case OOB_HW_FLOW_OFF:
+                fprintf(stderr, "Flow control OFF\n");
+                _serial_settings.is_hardware_flow_control = false;
+                _open_serial_port();
+                break;
+            case OOB_HW_FLOW_ON:
+                fprintf(stderr, "Flow control ON\n");
+                _serial_settings.is_hardware_flow_control = true;
+                _open_serial_port();
+                break;
+            default:
+                fprintf(stderr, "Unknown OOB command %d\n", oob_op);
+        }
+    }
+    FD_SET(_connection_fd, &_master_except_set);
+}
+
 int main(int argc, char** argv)
 {
-
-    bool is_hardware_flow_control = true;
+    FD_ZERO (&_master_read_set);
+    FD_ZERO (&_master_except_set);
     uint16_t port = 8888;
-    uint32_t baud_bps = 115200;
-    char* serial_port = strdup(DEFAULT_SERIAL_PORT);
+    _serial_settings.is_hardware_flow_control = true;
+    _serial_settings.baud_bps = 115200;
+    _serial_settings.device = strdup(DEFAULT_SERIAL_PORT);
     opterr = 0;
+
+    signal(SIGPIPE, SIG_IGN);
 
     int c;
     while ((c = getopt (argc, argv, "fp:d:b:")) != -1) {
         switch(c) {
             case 'f':
-                is_hardware_flow_control = false;
+                _serial_settings.is_hardware_flow_control = false;
                 break;
             case 'p':
                 port = atoi(optarg);
                 break;
             case 'd':
-                free(serial_port);
-                serial_port = strdup(optarg);
+                free(_serial_settings.device);
+                _serial_settings.device = strdup(optarg);
                 break;
             case 'b':
-                baud_bps = atoi(optarg);
+                _serial_settings.baud_bps = atoi(optarg);
                 break;
             case '?':
             default:
@@ -87,14 +154,12 @@ int main(int argc, char** argv)
         }
     }
 
-    fprintf(stderr, "serialgateway: port %d, serial=%s, baud=%d, flow=%s\n",
-            port, serial_port, baud_bps, (is_hardware_flow_control)?"HW":"sw");
+    fprintf(stderr, "serialgateway " VERSION
+                    ": port %d, serial=%s, baud=%d, flow=%s\n",
+            port, _serial_settings.device, _serial_settings.baud_bps,
+            (_serial_settings.is_hardware_flow_control)?"HW":"sw");
 
-    _serial_fd = serial_port_open(serial_port, baud_bps,
-                                  is_hardware_flow_control);
-    if (_serial_fd==-1) {
-        _error_exit("Could not open serial port.");
-    }
+    _open_serial_port();
 
     // Create listening socket
     int listen_sock;
@@ -110,7 +175,6 @@ int main(int argc, char** argv)
         _error_exit("setsockopt(SO_REUSEADDR) failed");
     }
 
-
     name.sin_family = AF_INET;
     name.sin_port = htons (port);
     name.sin_addr.s_addr = htonl (INADDR_ANY);
@@ -122,19 +186,28 @@ int main(int argc, char** argv)
         _error_exit("listen");
     }
 
-    FD_ZERO (&_master_read_set);
     FD_SET (listen_sock, &_master_read_set);
-    FD_SET (_serial_fd, &_master_read_set);
+
     int i;
     for(;;)
     {
-        fd_set read_fd_set = _master_read_set;
-        if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
+        _read_fd_set = _master_read_set;
+        _except_fd_set = _master_except_set;
+        if (select (FD_SETSIZE, &_read_fd_set, NULL, &_except_fd_set, NULL) < 0)
+        {
             _error_exit("select");
         }
 
         for (i = 0; i < FD_SETSIZE; ++i) {
-            if (FD_ISSET (i, &read_fd_set)) {
+            if (FD_ISSET (i, &_except_fd_set)) {
+                FD_CLR(i, &_master_except_set);
+                if (_connection_fd==i && sockatmark(i)==1) {
+                    LOG_DEBUG("Socket exceptfd %d", 1);
+                    _handle_oob_command();
+                }
+            }
+
+            if (FD_ISSET (i, &_read_fd_set)) {
                 if (i==listen_sock) {
                     struct sockaddr_in clientname;
                     socklen_t size = sizeof (clientname);
@@ -146,14 +219,15 @@ int main(int argc, char** argv)
                     _close_connectionfd();
                     _set_status_led(1);
                     fprintf (stderr,
-                         "Connect from host %s\n",
-                         inet_ntoa (clientname.sin_addr));
+                         "Connect from host %s fd=%d\n",
+                         inet_ntoa (clientname.sin_addr), new);
                     int enable = 1;
                     if (setsockopt(new, SOL_SOCKET, SO_KEEPALIVE,
                                    &enable, sizeof(enable)) < 0) {
                         fprintf (stderr, "Failed to set SO_KEEPALIVE");
                     }
                     FD_SET (new, &_master_read_set);
+                    FD_SET (new, &_master_except_set);
                     _connection_fd = new;
 
                 } else if (i==_serial_fd) {
@@ -177,11 +251,18 @@ int main(int argc, char** argv)
                         if (write(_serial_fd, _buf, len) < 0) {
                             _error_exit("write serial");
                         }
+
+                        if (sockatmark(_connection_fd)==1) {
+                            _handle_oob_command();
+                        }
+
                     }
+
                 } else {
                      fprintf(stderr, "Bug: Closing orphaned fd %d.\n", i);
                      close(i);
                      FD_CLR(i, &_master_read_set);
+                     FD_CLR(i, &_master_except_set);
                 }
 
             }
